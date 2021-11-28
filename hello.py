0b878103592685bdf31ca005b7ef2d37bec017ae
Himesh S <himeshsylesh27@gmail.com>
2021-11-29 00:18:30 +0530

    changes

--- hello.py
+++ hello.py
@@ -1,6 +1,15 @@
+#!/usr/bin/env python3
 """
 Example hello world 
 """
+import platform
+
 print("Hello World!!!")
 name = input("What is your name")
 print("Welcome!!, ", name)
+
+# binding of variable in string
+print("This is a python version {}". format(platform.python_version())) 
+
+#from Python 3.6 - F stands for formatting
+print(f'WELCOME!!! {name}')
